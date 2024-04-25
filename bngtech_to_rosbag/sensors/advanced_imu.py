11287f1df5f7b983bf11b2729e677be960c0c631
Shibodd <simonebondi456@gmail.com>
2024-04-25 11:50:51 +0200

    Uniform sensor format

--- bngtech_to_rosbag/sensors/advanced_imu.py
+++ bngtech_to_rosbag/sensors/advanced_imu.py
@@ -14,15 +14,16 @@ def quat_from_axes(x, y, z):
 
 
 class AdvancedIMUSensor:
-  def __init__(self, typestore: Typestore, IMU: beamngpy.sensors.AdvancedIMU):
+  def __init__(self, typestore: Typestore, sensor_name, IMU: beamngpy.sensors.AdvancedIMU):
     self.typestore = typestore
+    self.sensor_name = sensor_name
     self.IMU = IMU
 
   def parse_sample(self, sample):
     ns, stamp = msg_helpers.stamp_from_float(sample['time'], self.typestore)
     
     return ns, self.typestore.types['sensor_msgs/msg/Imu'](
-      header = msg_helpers.header(self.typestore, 'map', stamp),
+      header = msg_helpers.header(self.typestore, 'imu_link', stamp),
       orientation = msg_helpers.quaternion(self.typestore,
         quat_from_axes(sample['dirX'], sample['dirY'], sample['dirZ'])
       ),
@@ -36,5 +37,5 @@ class AdvancedIMUSensor:
   def poll_msgs(self):
     data = self.IMU.poll()
     if not isinstance(data, dict):
-      return []
-    return [self.parse_sample(x) for x in data.values()]
+      return { self.sensor_name: [] }
+    return { self.sensor_name: [self.parse_sample(x) for x in data.values()] }
--- bngtech_to_rosbag/sensors/classic_sensors.py
+++ bngtech_to_rosbag/sensors/classic_sensors.py
@@ -12,30 +12,27 @@ class ClassicSensors:
     self.vehicle.sensors.poll()
     time = self.vehicle.sensors[self.timer_name]['time']
     return {
-      k: parser(self.typestore, time, self.vehicle.sensors[k])
-      for k, parser in self.sensors.items()
+      k: [ npp[1](self.typestore, time, self.vehicle.sensors[npp[0]]) ]
+      for k, npp in self.sensors.items()
     }
-  
-import numpy as np
 
-def vehicle_state_odometry(typestore: Typestore, time, sample):
+def odometry_pose(typestore: Typestore, time, sample):
   ns, stamp = msg_helpers.stamp_from_float(time, typestore)
 
-  return typestore.types['nav_msgs/msg/Odometry'](
-    child_frame_id = 'map',
-    header = msg_helpers.header(typestore, 'map', stamp),
-    pose = typestore.types['geometry_msgs/msg/PoseWithCovariance'](
-      pose = typestore.types['geometry_msgs/msg/Pose'](
-        position = msg_helpers.point(typestore, sample['pos']),
-        orientation = msg_helpers.quaternion(typestore, sample['rotation'])
-      ),
-      covariance = np.zeros((36,), dtype=np.float64)
-    ),
-    twist = typestore.types['geometry_msgs/msg/TwistWithCovariance'](
-      twist = typestore.types['geometry_msgs/msg/Twist'](
-        linear = msg_helpers.vector3(typestore, sample['vel']),
-        angular = msg_helpers.vector3(typestore, [0,0,0])
-      ),
-      covariance = np.zeros((36,), dtype=np.float64)
+  return ns, typestore.types['geometry_msgs/msg/PoseStamped'](
+    header = msg_helpers.header(typestore, 'track', stamp),
+    pose = typestore.types['geometry_msgs/msg/Pose'](
+      position = msg_helpers.point(typestore, sample['pos']),
+      orientation = msg_helpers.quaternion(typestore, sample['rotation'])
     )
+  )
+
+def twist_wheelspeed(typestore: Typestore, time, sample):
+  ns, stamp = msg_helpers.stamp_from_float(time, typestore)
+  return ns, typestore.types['geometry_msgs/msg/TwistStamped'](
+    header = msg_helpers.header(typestore, 'imu_link', stamp),
+    twist = typestore.types['geometry_msgs/msg/Twist'](
+      linear = msg_helpers.vector3(typestore, [sample['wheelspeed'], 0, 0]),
+      angular = msg_helpers.vector3(typestore, [0, 0, 0])
+    ),
   )
