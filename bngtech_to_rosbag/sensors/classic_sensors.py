1e8a8e1bcdc5bdb2d85db05546134ebaec95c748
Shibodd <simonebondi456@gmail.com>
2024-04-26 11:13:42 +0200

    Add vehicle TF

--- bngtech_to_rosbag/sensors/classic_sensors.py
+++ bngtech_to_rosbag/sensors/classic_sensors.py
@@ -20,17 +20,33 @@ def odometry_pose(typestore: Typestore, time, sample):
   ns, stamp = msg_helpers.stamp_from_float(time, typestore)
 
   return ns, typestore.types['geometry_msgs/msg/PoseStamped'](
-    header = msg_helpers.header(typestore, 'track', stamp),
+    header = msg_helpers.header(typestore, 'world', stamp),
     pose = typestore.types['geometry_msgs/msg/Pose'](
       position = msg_helpers.point(typestore, sample['pos']),
       orientation = msg_helpers.quaternion(typestore, sample['rotation'])
     )
   )
 
+def vehicle_tf(typestore: Typestore, time, sample):
+  ns, stamp = msg_helpers.stamp_from_float(time, typestore)
+
+  return ns, typestore.types['tf2_msgs/msg/TFMessage'](
+    transforms = [
+      typestore.types['geometry_msgs/msg/TransformStamped'](
+        header = msg_helpers.header(typestore, 'world', stamp),
+        child_frame_id = 'vehicle',
+        transform = typestore.types['geometry_msgs/msg/Transform'](
+          translation = msg_helpers.vector3(typestore, sample['pos']),
+          rotation = msg_helpers.quaternion(typestore, sample['rotation'])
+        )
+      )
+    ]
+  )
+
 def twist_wheelspeed(typestore: Typestore, time, sample):
   ns, stamp = msg_helpers.stamp_from_float(time, typestore)
   return ns, typestore.types['geometry_msgs/msg/TwistStamped'](
-    header = msg_helpers.header(typestore, 'imu_link', stamp),
+    header = msg_helpers.header(typestore, 'vehicle', stamp),
     twist = typestore.types['geometry_msgs/msg/Twist'](
       linear = msg_helpers.vector3(typestore, [sample['wheelspeed'], 0, 0]),
       angular = msg_helpers.vector3(typestore, [0, 0, 0])
